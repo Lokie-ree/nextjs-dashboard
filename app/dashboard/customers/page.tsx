e569ac77cc75fbd651d9ca544e03c613ffee0d14
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-08-14 13:31:50 -0500

    added routes to Customer Page and Invoices Page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Customers = () => {
+  return <p>Customers Page</p>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Invoices = () => {
+  return <p>Invoices Page</p>;
+};
+
+export default Invoices;
